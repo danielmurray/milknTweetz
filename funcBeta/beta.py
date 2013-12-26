7101caa69ac64420a59e4078a4795feaa2904934
Dan <daniel.phillip.murray@gmail.com>
2013-12-27 01:17:23 +0800

    added beta test code

--- funcBeta/beta.py
+++ funcBeta/beta.py
@@ -21,11 +21,12 @@ class Spam(Base):
 
 class Beta():
 
-	def __init__(self, tweet_text ):
+	def __init__(self, tweet_text, db ):
+		self.session = db
 		self.laplace_k = 1
 		self.tweet = self.format_tweet(tweet_text)
-		self.total_ham_count = session.query(func.sum(Ham.count)).scalar()
-		self.total_spam_count = session.query(func.sum(Spam.count)).scalar()
+		self.total_ham_count = self.session.query(func.sum(Ham.count)).scalar()
+		self.total_spam_count = self.session.query(func.sum(Spam.count)).scalar()
 		self.total_count = self.total_ham_count + self.total_spam_count
 		self.ham_chance = self.total_ham_count / self.total_count
 		# print "ham_chance", self.ham_chance
@@ -34,10 +35,18 @@ class Beta():
 		self.words = []
 		self.score = 0
 		for word in self.tweet:
-			word_score = self.naive_bayes(word)
+			word_score = self.even_more_naive(word)
 			self.words.append((word, word_score))
 			self.score = self.score + word_score
-			print self.score, '++Positive++' if self.score > 0 else '--Negative--', word
+			# print self.score, '++Positive++' if self.score > 0 else '--Negative--', word
+
+	def even_more_naive(self, word):
+		ham_count = self.get_word_count(word, Ham)
+		spam_count = self.get_word_count(word, Spam)
+		ham = ham_count /self.total_ham_count
+		spam = spam_count /self.total_spam_count
+		score = ham - spam
+		return float(score)
 
 	def naive_bayes(self, word):
 		ham_count = self.get_word_count(word, Ham)
@@ -47,7 +56,7 @@ class Beta():
 		ham = ham_smoothed /self.total_ham_count
 		spam = spam_smoothed /self.total_spam_count
 		score = ham / ( ham+spam )
-		return 2*float(score) - 1
+		return float(score)-0.5
 
 	def format(self, word):
 		lowercase = word.lower()
@@ -63,7 +72,7 @@ class Beta():
 		return formatted_words
 
 	def get_word_count(self, word, lexicon):
-		lexicon_word = session.query(lexicon).filter_by(word=word).first()
+		lexicon_word = self.session.query(lexicon).filter_by(word=word).first()
 		if lexicon_word is None:
 			return 0
 		else:
@@ -96,8 +105,8 @@ if __name__ == "__main__":
 
 	SessionClass = sessionmaker(bind=db)
 	session = SessionClass()
-	# tweet = Beta("So.... after some very long time of getting used to and playing around. The switch to Windows 8.1 is kind of on track now. Even though not very intuitive, the tablet interface is actually quite nice. I mean, not necessary the most useful, but swiping between apps is pretty cool. I'm still not sure if the tablet/PC interface hybrid is a good idea. It's quite confusing to have a \"dorpbox application\" and a \"dropbox app\" in my opinion. Not feasible at the moment, but making a whole switch to just tablet interface at once might be better (as in no more traditional interface, just the new one). Kind of look forward to the day that happens. Shop needs to load faster, and is still lacking quite some official apps, but its fine. I have to give it to facebook for always doing a \"quick support\" on things now a days though. Lots more bugs and glitches to be fix, but all major functions usable? Not sure about the all-in-one-Windows approach but it could be a good solution, at least I've been searching for such solution for quite some time. So far, I think the only company that has sucessfully provide a all-in-one solution is Apple (though, never tried). Microsoft could be alright as long as they \"DO SUPPORT OFFLINE MODE\". Been trying to integrate google, faceboook, microsoft all together, not quite possible yet.")
-	tweet = Beta("This is a great product but I can\'t fit it in my asshole so therefore I think it is a bad product, you should not buy it.")	
+	# tweet = Beta("These are, overall amazing headphones. They sound great, are comfortable, and easily driven by portable players. However, when I first bought them, and eagerly unwrapped the packaging and tried them out, I was bitterly dissapointed. Where had my money gone, I wondered. They sounded tin canny, and the bass had no punch. I nearly returned them. But, taking the advice of many reviewers, I plugged them into my computer and left the music playing loudly for nearly a week straight. And it paid off. After nearly a month's use, these headphones sound GREAT. I have fallen in love witht eh tight, clear treble and amazingly accurate bass that the HD-280s provide. Jazz and classical fans (and rockers too but to a lesser extent) will love these headphones. I could go on and on and on about the sound quality, but you would find your self reading needless repition of the words \"great,\" \"amazing,\" etcetc. The design is a little big, but very comfy. I can wear these for several hours at a time and my ears will feel fine. The way that they fold up is also very useful for the frequent traveler, and the stretchy wire design is also very handy, keeping the wire out of the way yet providing you nearly 3 meters worth of wire (if stretched to the max). I would also just like to note that, after several airplane trips, the noise reduction in these headphones is also wonderful, and the HD-280 Pros make airplane flights that much more enjoyable. In conclusion, wait at LEAST two weeks, preferably more before judging these headphones, because the more you use them, the better they sound. And after a month's worth of use, they sound great. And they also fit great, and are extremely comfortable. The price too, isn't too bad, and I found them at ... for [$$$], including shipping from the states to taiwan. Do your ears a favor, and buy a pair of HD 280s.")
+	tweet = Beta("bad good love hate a across am an and any are as at be been being but by can could did do does each for from had has have in into is isn't it it'd it'll it's its of on or that that's thats the there there's theres these this those to under until up were will with would ")
 	print tweet.get_score()
 
 
